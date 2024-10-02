2f96098c7f93322c39db70b7bb42aeb9fea8917a
saeed221373 <saeed221373@googlemail.com>
2024-10-02 20:56:32 +0000

    fix navigation

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -14,7 +14,7 @@ function Nav() {
         <ul className='nav_link'>
           <li className='nav-text'><a href="/">Home</a></li>
           <li className='nav-text'><a href="/about">About</a></li>
-          <li className='nav-text'><a href="/contact">Contact</a></li>
+          <li className='nav-text'><a href="https://basescan.org/address/0x86638bed40fb4a7cd9ef1666b5d5971f4964dd19">Contract</a></li>
         </ul>
 
         <div className="button-container">

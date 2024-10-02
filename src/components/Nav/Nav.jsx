59dd9c1af2dda8a66a4cbacdeded146ce3a38440
saeed221373 <saeed221373@googlemail.com>
2024-10-02 16:23:00 +0000

    make button center

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './Nav.css'
 
+
 function Nav() {
   return (
     <header className="header">
       <nav className="nav">
         <div className="brand">
-        <img src='./base.svg' className="logo" />
-        <h3>Base Public Chat</h3>
+          <img src='./base.svg' className="logo" />
+          <h3>Base Public Chat</h3>
         </div>
-        
+
         <ul className='nav_link'>
-          <li className= 'nav-text'><a href="/">Home</a></li>
-          <li className= 'nav-text'><a href="/about">About</a></li>
-          <li className= 'nav-text'><a href="/contact">Contact</a></li>
+          <li className='nav-text'><a href="/">Home</a></li>
+          <li className='nav-text'><a href="/about">About</a></li>
+          <li className='nav-text'><a href="/contact">Contact</a></li>
         </ul>
 
         <div className="button-container">
-        <button className="button">Button Text</button>
-      </div>
+          <button className="button">Button Text</button>
+        </div>
       </nav>
-      
     </header>
   );
 }

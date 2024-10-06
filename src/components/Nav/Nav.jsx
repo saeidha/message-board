47bf70041be759f8c85efd6df8f01650faf37bd4
saeed221373 <saeed221373@googlemail.com>
2024-10-06 05:51:33 +0000

    fix car message add farcaster account

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -13,8 +13,8 @@ function Nav({ handleConnect, chainId, chainHex, chainName, rpcUrl }) {
 
         <ul className='nav_link'>
           <li className='nav-text'><a href="/">Home</a></li>
-          <li className='nav-text'><a href="/about">About</a></li>
           <li className='nav-text'><a href="https://basescan.org/address/0x86638bed40fb4a7cd9ef1666b5d5971f4964dd19">Contract</a></li>
+          <li className='nav-text'><a href="https://warpcast.com/aichaindev">About</a></li>
         </ul>
         <div className="button-container">
             <ConnectButton
